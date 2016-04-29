db8b5c24049fdf12466c5a6e9642b9c8f3b8e7af
Håvard Lian <haavarhl@stud.ntnu.no>
2016-04-29 13:46:24 +0200

    Code cleanup

--- p0sx/urls.py
+++ p0sx/urls.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 from pos.views.shift import ShiftViewSet
 from pos.views.stock import CategoryViewSet, IngredientViewSet, ItemViewSet, OrderLineViewSet, OrderViewSet, PurchaseViewSet

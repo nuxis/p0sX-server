f7e06ede86a1b7493d78d8cd92130f53f71a63ff
Kristoffer Dalby <kradalby@kradalby.no>
2016-06-25 19:30:17 +0200

    moves static folder to static/

--- p0sx/settings/base.py
+++ p0sx/settings/base.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'

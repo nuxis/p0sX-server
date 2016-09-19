d7e7385d73592076e9819376862a25c910d1c411
Kristoffer Dalby <kradalby@kradalby.no>
2016-09-19 19:53:23 +0200

    adds more fields to users

--- pos/models/user.py
+++ pos/models/user.py
@@ -3,8 +3,13 @@ from django.db import models
 
 class User(models.Model):
     card = models.CharField(max_length=255, unique=True, primary_key=True)
-    name = models.CharField(max_length=255)
-    max_credit = models.IntegerField()
+    credit = models.IntegerField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=12)
+    crew = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    email = models.EmailField()
 
     def __str__(self):
-        return self.name
+        return "{} {}".format(self.first_name, self.last_name)

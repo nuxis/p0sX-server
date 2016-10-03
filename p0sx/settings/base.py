2dbb5bfea3361f70bd981587467d08ed16461ddd
Kristoffer Dalby <kradalby@kradalby.no>
2016-10-03 12:09:58 +0200

    tags 1.0.0

--- p0sx/settings/base.py
+++ p0sx/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+VERSION = '1.0.0'
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 

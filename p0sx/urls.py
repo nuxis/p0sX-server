db1ce9bfbb9d3ee4c724e0a10cfc8d31e072b2ea
Håvard Lian <haavarhl@stud.ntnu.no>
2016-10-30 13:26:50 +0100

    Add crew report

--- p0sx/urls.py
+++ p0sx/urls.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import RedirectView
 
 from pos.views.crew import CrewViewSet
-from pos.views.littleadmin import check_credit, credit_edit, credit_overview, sale_overview
+from pos.views.littleadmin import check_credit, credit_edit, credit_overview, sale_overview, crew_report
 from pos.views.shift import AllShiftsViewSet, CurrentShiftViewSet, NewShiftViewSet, ShiftViewSet
 from pos.views.stock import (CategoryViewSet,
                              CreditCheckViewSet,
@@ -29,7 +29,8 @@ littleadmin_url = [
     url(r'check/', check_credit, name='check'),
     url(r'overview/', credit_overview, name='overview'),
     url(r'edit/(?P<card>\w+)', credit_edit, name='edit'),
-    url(r'sale/', include(sale_url, namespace='sale'))
+    url(r'sale/', include(sale_url, namespace='sale')),
+    url(r'crew_report/', crew_report, name="crew_report")
 ]
 
 # Routers provide an easy way of automatically determining the URL conf.
